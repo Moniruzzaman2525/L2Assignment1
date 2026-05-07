bfc12cd71fd62862c740f5a92d5c426e4ae1b2ee
unknown <moniurzzaman25@gmail.com>
2026-05-07 18:31:06 +0600

    problem six

--- src/solutions.ts
+++ src/solutions.ts
@@ -75,3 +75,32 @@ const myBook = {
 const problemFive =  toggleReadStatus(myBook);
 console.log(problemFive)
 
+
+// Problem Six
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+const student = new Student("Alice", 20, "A");
+const problemSix = student.getDetails();
+console.log(problemSix)
