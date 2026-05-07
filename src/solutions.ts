6a73049147a5781497d642766101f892bd2a2ac1
unknown <moniurzzaman25@gmail.com>
2026-05-07 18:04:58 +0600

    Problem solve two

--- src/solutions.ts
+++ src/solutions.ts
@@ -9,3 +9,12 @@ function filterEvenNumbers(numbers: number[]): number[] {
 const problemOne = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 console.log(problemOne)
 
+
+// Problem Two
+
+function reverseString(input: string): string {
+  return input.split("").reverse().join("");
+}
+
+const problemTwo =  reverseString("typescript");
+console.log(problemTwo)
