b6a0529c749db9a927f42b37c1a00148c2f91aa5
unknown <moniurzzaman25@gmail.com>
2026-05-07 18:10:16 +0600

    problem solve three

--- src/solutions.ts
+++ src/solutions.ts
@@ -18,3 +18,18 @@ function reverseString(input: string): string {
 
 const problemTwo =  reverseString("typescript");
 console.log(problemTwo)
+
+
+// Problem Three
+
+type StringOrNumber = string | number;
+
+function checkType(input: StringOrNumber): "String" | "Number" {
+  return typeof input === "string" ? "String" : "Number";
+}
+
+const problemThree1 =  checkType("Hello");
+console.log(problemThree1)
+
+const problemThree2 = checkType(42);
+console.log(problemThree2)
